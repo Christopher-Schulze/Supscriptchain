77cebaf7018e23656ce9870617f3c8ecefec4df2
Christopher <schulze.christopher@icloud.com>
2025-07-04 00:59:29 +0200

    Configure ESLint with TypeScript and add missing plugins

--- eslint.config.js
+++ eslint.config.js
@@ -1,1 +1,24 @@
-module.exports = require('./.eslintrc.json');
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+const tsParser = require('@typescript-eslint/parser');
+const js = require('@eslint/js');
+module.exports = [
+  {
+    files: ['**/*.ts'],
+    ignores: ['dist/**', 'node_modules/**'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...tsPlugin.configs.recommended.rules,
+    },
+  },
+];
