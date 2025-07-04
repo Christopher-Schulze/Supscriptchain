b6129618a62d4b77baa8503076c28b50b86250d9
Christopher <schulze.christopher@icloud.com>
2025-07-04 13:01:52 +0200

    chore(ci): add gas report

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,13 +1,13 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
-import "@nomicfoundation/hardhat-verify";
-import "@openzeppelin/hardhat-upgrades";
-import "@typechain/hardhat";
-import "hardhat-gas-reporter";
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import '@nomicfoundation/hardhat-verify';
+import '@openzeppelin/hardhat-upgrades';
+import '@typechain/hardhat';
+import 'hardhat-gas-reporter';
 
-import path from "path";
-import * as dotenv from "dotenv";
-import "./tasks/upgrade";
+import path from 'path';
+import * as dotenv from 'dotenv';
+import './tasks/upgrade';
 
 dotenv.config();
 
@@ -15,30 +15,36 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.26",
-        path: path.resolve(__dirname, "node_modules/solc/soljson.js"),
+        version: '0.8.26',
+        path: path.resolve(__dirname, 'node_modules/solc/soljson.js'),
       },
     ],
   },
   typechain: {
-    outDir: "typechain",
-    target: "ethers-v6",
+    outDir: 'typechain',
+    target: 'ethers-v6',
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY || "",
+    apiKey: process.env.ETHERSCAN_API_KEY || '',
   },
   gasReporter: {
     enabled: true,
+    outputFile: 'gas-report.txt',
+    noColors: true,
   },
   networks: {
     // Example settings reading RPC URLs and keys from `.env`
     sepolia: {
-      url: process.env.SEPOLIA_RPC_URL || "",
-      accounts: process.env.SEPOLIA_PRIVATE_KEY ? [process.env.SEPOLIA_PRIVATE_KEY] : [],
+      url: process.env.SEPOLIA_RPC_URL || '',
+      accounts: process.env.SEPOLIA_PRIVATE_KEY
+        ? [process.env.SEPOLIA_PRIVATE_KEY]
+        : [],
     },
     mainnet: {
-      url: process.env.MAINNET_RPC_URL || "",
-      accounts: process.env.MAINNET_PRIVATE_KEY ? [process.env.MAINNET_PRIVATE_KEY] : [],
+      url: process.env.MAINNET_RPC_URL || '',
+      accounts: process.env.MAINNET_PRIVATE_KEY
+        ? [process.env.MAINNET_PRIVATE_KEY]
+        : [],
     },
   },
 };
